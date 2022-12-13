eee4f7bb96947a1c41e8baef2c13d093ef7cf91a
Lurinoxi <75939435+Lurinoxi@users.noreply.github.com>
2022-12-13 11:56:58 +0100

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -5,9 +5,9 @@
     //Constant Erstellen damit sich die Values nicht wiederholen
     define('SITEURL', 'http://localhost/food/');
     define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
+    define('DB_USERNAME', '');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'Mama Bringts'); 
+    define('DB_NAME', ''); 
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error($conn)); //zur Datenbank verbinden
     $db_select = mysqli_select_db($conn , DB_NAME) or die(mysqli_error($conn)); //Datenbank auswählen
