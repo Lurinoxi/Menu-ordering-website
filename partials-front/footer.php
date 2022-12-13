e30cc5ace4c1ff50d3d69756d61e5d23d873dc12
Lurinoxi <75939435+Lurinoxi@users.noreply.github.com>
2022-12-13 11:55:20 +0100

    footer n menu
    footer n menu

---
+++ partials-front/footer.php
@@ -0,0 +1,10 @@
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p></a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+    </body>
+</html>
