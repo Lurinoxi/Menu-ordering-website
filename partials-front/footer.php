89f230dabab182ee28fd32aee08a94f6bee7a70d
Lurinoxi <75939435+Lurinoxi@users.noreply.github.com>
2023-05-08 12:00:39 +0200

    Update footer.php

--- partials-front/footer.php
+++ partials-front/footer.php
@@ -1,7 +1,7 @@
     <!-- footer Section Starts Here -->
     <section class="footer">
         <div class="container text-center">
-            <p>All rights reserved, Mama Bringts. Developed by <a href="https://www.sie.at/">Matteo Lang</a></p>
+            <p>All rights reserved, Mama Bringts. Developed by <a href="https://github.com/Lurinoxi">Lurinoxi</a></p>
         </div>
         
         <!-- Email Reminder Notification Popup Starts Here -->
